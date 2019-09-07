44a5ff6f54ec1b58d42d93a6f7b3c58b8dbb1fcc
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-09-07 17:15:19 -0300

    cliente enviando texto pro servidor

--- lab2/Client.py
+++ lab2/Client.py
@@ -1,86 +1,108 @@
 import os
-
-
-def is_a_valid_command_line(command_line):
-    command_args = command_line.split(" ")
-    command = command_args[0]
-    n_args = len(command_args)
-
-    # Directory Browsing and Listing
-    if command == "cd" \
-            or command == "mkdir" or command == "rmdir" \
-            or command == "get" or command == "put" or command == "delete" \
-            or command == "open":
-        if n_args != 2:
-            print("Error: 1 argument is expected")
-        return n_args == 2
-
-    elif command == "pwd" or command == "close" or command == "quit":
-        if n_args != 1:
-            print("Error: No argument is expected")
-        return n_args == 1
-
-    elif command == "ls":
-        if n_args != 1 and n_args != 2:
-            print("Error: 0 or 1 argument is expected")
-        return n_args == 1 or n_args == 2
-
-    else:
-        print("Invalid command")
-        return False
+import socket
 
 
 if __name__ == "__main__":
-    current_dir_path = os.getcwd()
-    print("current directory is : " + current_dir_path)
-    folder_name = os.path.basename(current_dir_path)
-    print("Directory name is : " + folder_name)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 2122)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
 
     while True:
         command_line = input()
 
-        if is_a_valid_command_line(command_line):
-            command_args = command_line.split(" ")
-            command = command_args[0]
+        # Send data
+        print("enviando: " + str(command_line))
+        sock.sendall(bytes(command_line, 'utf-8'))
 
-            # Directory Browsing and Listing
-            if command == "cd":
-                dirname = command_args[1]
-                print(dirname)
+        # # Create a TCP/IP socket
+        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #
+        # # Connect the socket to the port where the server is listening
+        # server_address = ('localhost', 2122)
+        # print('connecting to {} port {}'.format(*server_address))
+        # sock.connect(server_address)
 
-            elif command == "ls":
-                if len(command_args) != 1:
-                    dirname = command_args[1]
-                    print(dirname)
-
-            elif command == "pwd":
-                print(current_dir_path)
-
-            # Directory manipulation
-            elif command == "mkdir":
-                dirname = command_args[1]
-                print(dirname)
-
-            elif command == "rmdir":
-                dirname = command_args[1]
-                print(dirname)
-
-            # File Handling
-            elif command == "get":
-                filename = command_args[1]
-                print(filename)
-            elif command == "put":
-                filename = command_args[1]
-                print(filename)
-            elif command == "delete":
-                filename = command_args[1]
-                print(filename)
-
-            # Connection Management
-            elif command == "close":
-                pass
-            elif command == "open":
-                filename = command_args[1]
-                print(filename)
-            elif command == "quit":
-                break
+        # if is_a_valid_command_line(command_line):
+        #     command_args = command_line.split(" ")
+        #     command = command_args[0]
+        #
+        #     # Directory Browsing and Listing
+        #     if command == "cd":
+        #         dirname = command_args[1]
+        #         print(dirname)
+        #
+        #     elif command == "ls":
+        #         if len(command_args) != 1:
+        #             dirname = command_args[1]
+        #             print(dirname)
+        #
+        #     elif command == "pwd":
+        #         pass
+        #
+        #     # Directory manipulation
+        #     elif command == "mkdir":
+        #         dirname = command_args[1]
+        #         print(dirname)
+        #
+        #     elif command == "rmdir":
+        #         dirname = command_args[1]
+        #         print(dirname)
+        #
+        #     # File Handling
+        #     elif command == "get":
+        #         filename = command_args[1]
+        #         print(filename)
+        #     elif command == "put":
+        #         filename = command_args[1]
+        #         print(filename)
+        #     elif command == "delete":
+        #         filename = command_args[1]
+        #         print(filename)
+        #
+        #     # Connection Management
+        #     elif command == "close":
+        #         pass
+        #     elif command == "open":
+        #         server = command_args[1]
+        #         # if server == "server.ita.br":
+        #         if True:
+        #             # Create a TCP/IP socket
+        #             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #             # Connect the socket to the port where the server is listening
+        #             server_address = ('localhost', 2122)
+        #             print('connecting to {} port {}'.format(*server_address))
+        #             sock.connect(server_address)
+        #
+        #             # Receive the data in small chunks and retransmit it
+        #             while True:
+        #                 data = sock.recv(16)
+        #                 if data:
+        #                     print(data)
+        #                 else:
+        #                     print('no data from', client_address)
+        #                     break
+        #
+        #             user = input()
+        #             sock.sendall(bytes(user, 'utf-8'))
+        #
+        #             # Receive the data in small chunks and retransmit it
+        #             while True:
+        #                 data = sock.recv(16)
+        #                 if data:
+        #                     print(data)
+        #                 else:
+        #                     print('no data from', client_address)
+        #                     break
+        #
+        #             password = input()
+        #             sock.sendall(bytes(password, 'utf-8'))
+        #
+        #         else:
+        #             print("No server " + server)
+        #
+        #     elif command == "quit":
+        #         break
---
+++ lab2/Server.py
@@ -0,0 +1,105 @@
+import socket
+import _thread
+import sys
+
+
+def is_a_valid_command_line(command_line):
+    command_args = command_line.split(" ")
+    command = command_args[0]
+    n_args = len(command_args)
+
+    # Directory Browsing and Listing
+    if command == "cd" \
+            or command == "mkdir" or command == "rmdir" \
+            or command == "get" or command == "put" or command == "delete" \
+            or command == "open":
+        if n_args != 2:
+            print("Error: 1 argument is expected")
+        return n_args == 2
+
+    elif command == "pwd" or command == "close" or command == "quit":
+        if n_args != 1:
+            print("Error: No argument is expected")
+        return n_args == 1
+
+    elif command == "ls":
+        if n_args != 1 and n_args != 2:
+            print("Error: 0 or 1 argument is expected")
+        return n_args == 1 or n_args == 2
+
+    else:
+        print("Invalid command")
+        return False
+
+
+def test_thread(some_text):
+    print("example: " + str(some_text))
+
+
+if __name__ == "__main__":
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to the port
+    server_address = ('localhost', 2122)
+    print('starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print('waiting for a connection')
+        _thread.start_new(test_thread, ("oi",))
+        connection, client_address = sock.accept()
+
+        try:
+            print('connection from', client_address)
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print("recebido: " + data.decode("utf-8"))
+                if not data:
+                #     print('sending data back to the client')
+                #     connection.sendall(data)
+                # else:
+                    print('no data from', client_address)
+                    break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+
+        # try:
+        #     print('connection from', client_address)
+        #     connection.sendall(bytes("user:", 'utf-8'))
+        #
+        #     # Receive the data in small chunks and retransmit it
+        #     while True:
+        #         data = connection.recv(16)
+        #         print('received {!r}'.format(data))
+        #         if data:
+        #             print('sending data back to the client')
+        #             connection.sendall(data)
+        #         else:
+        #             print('no data from', client_address)
+        #             break
+        #
+        #     connection.sendall(bytes("password:", 'utf-8'))
+        #     # Receive the data in small chunks and retransmit it
+        #     while True:
+        #         data = connection.recv(16)
+        #         print('received {!r}'.format(data))
+        #         if data:
+        #             print('sending data back to the client')
+        #             connection.sendall(data)
+        #         else:
+        #             print('no data from', client_address)
+        #             break
+        #
+        # finally:
+        #     # Clean up the connection
+        #     connection.close()
