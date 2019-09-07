dce653666d2aaa29c7f7b96ded9fbb1411b16829
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-09-07 17:57:45 -0300

    command line sendo recebida

--- lab2/Server.py
+++ lab2/Server.py
@@ -3,6 +3,21 @@ import _thread
 import sys
 
 
+def control_connection(connection, client_address):
+    print('connection from', client_address)
+
+    while True:
+        command_line = ""
+        while True:
+            data = connection.recv(16)
+            print("recebido: " + data.decode("utf-8"))
+            command_line = command_line + data.decode("utf-8")
+            if len(data.decode("utf-8")) < 16:
+                break
+        print("command_line: " + str(command_line))
+        print(is_a_valid_command_line(command_line))
+
+
 def is_a_valid_command_line(command_line):
     command_args = command_line.split(" ")
     command = command_args[0]
@@ -14,17 +29,17 @@ def is_a_valid_command_line(command_line):
             or command == "get" or command == "put" or command == "delete" \
             or command == "open":
         if n_args != 2:
-            print("Error: 1 argument is expected")
+            print("Error: 1 argument is expected to '" + command + "'")
         return n_args == 2
 
     elif command == "pwd" or command == "close" or command == "quit":
         if n_args != 1:
-            print("Error: No argument is expected")
+            print("Error: No argument is expected to '" + command + "'")
         return n_args == 1
 
     elif command == "ls":
         if n_args != 1 and n_args != 2:
-            print("Error: 0 or 1 argument is expected")
+            print("Error: 0 or 1 argument is expected to '" + command + "'")
         return n_args == 1 or n_args == 2
 
     else:
@@ -54,21 +69,22 @@ if __name__ == "__main__":
         print('waiting for a connection')
         _thread.start_new(test_thread, ("oi",))
         connection, client_address = sock.accept()
+        _thread.start_new_thread(control_connection, (connection, client_address))
 
-        try:
-            print('connection from', client_address)
+        # try:
+        # print('connection from', client_address)
+        #
+        # # Receive the data in small chunks and retransmit it
+        # command_line = ""
+        # while True:
+        #     data = connection.recv(16)
+        #     print("recebido: " + data.decode("utf-8"))
+        #     command_line = command_line + data.decode("utf-8")
+        #     if len(data.decode("utf-8")) < 16:
+        #         break
+        # print("command_line: " + str(command_line))
+        # print(is_a_valid_command_line(command_line))
 
-            # Receive the data in small chunks and retransmit it
-            message = ""
-            i = 0
-            while True:
-                data = connection.recv(16)
-                print("recebido: " + data.decode("utf-8"))
-                message = message + data.decode("utf-8")
-                if len(data.decode("utf-8")) < 16:
-                    break
-            print("mensagem: " + str(message))
-
-        finally:
-            # Clean up the connection
-            connection.close()
+        # finally:
+        #     # Clean up the connection
+        #     connection.close()
