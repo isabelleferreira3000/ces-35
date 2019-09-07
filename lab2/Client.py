2e38ef64529e07adf6060d7395428fcf0dbd898d
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-09-07 18:29:00 -0300

    comeco autenticacao

--- lab2/Client.py
+++ lab2/Client.py
@@ -2,21 +2,66 @@ import os
 import socket
 
 
+def receive_message(connection):
+    message_received = ""
+    while True:
+        data_received = connection.recv(16)
+        # print("recebido: " + data_received.decode("utf-8"))
+        message_received = message_received + data_received.decode("utf-8")
+        if len(data_received.decode("utf-8")) < 16:
+            break
+    return message_received
+
+
 if __name__ == "__main__":
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 2122)
-    print('connecting to {} port {}'.format(*server_address))
-    sock.connect(server_address)
-
     while True:
         command_line = input()
+        command_args = command_line.split(" ")
+        command = command_args[0]
+        n_args = len(command_args)
 
-        # Send data
-        print("enviando: " + str(command_line))
-        sock.sendall(bytes(command_line, 'utf-8'))
+        if command == "open":
+            # Create a TCP/IP socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            # Connect the socket to the port where the server is listening
+            server_address = ('localhost', 2122)
+            # print("connecting to " + server_address[0] + " port " + str(server_address[1]))
+            sock.connect(server_address)
+
+            # authentication
+            print(receive_message(sock))
+            username = input()
+            sock.sendall(bytes(username, 'utf-8'))
+            print(receive_message(sock))
+            password = input()
+            sock.sendall(bytes(password, 'utf-8'))
+
+            while True:
+                command_line = input()
+
+                # Send data
+                print("enviando: " + str(command_line))
+                sock.sendall(bytes(command_line, 'utf-8'))
+
+        else:
+            print("not connected")
+            pass
+
+    # # Create a TCP/IP socket
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #
+    # # Connect the socket to the port where the server is listening
+    # server_address = ('localhost', 2122)
+    # print('connecting to {} port {}'.format(*server_address))
+    # sock.connect(server_address)
+    #
+    # while True:
+    #     command_line = input()
+    #
+    #     # Send data
+    #     print("enviando: " + str(command_line))
+    #     sock.sendall(bytes(command_line, 'utf-8'))
 
         # # Create a TCP/IP socket
         # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- lab2/Server.py
+++ lab2/Server.py
@@ -3,19 +3,43 @@ import _thread
 import sys
 
 
-def control_connection(connection, client_address):
-    print('connection from', client_address)
-
+def receive_message(connection):
+    message_received = ""
     while True:
-        command_line = ""
-        while True:
-            data = connection.recv(16)
-            print("recebido: " + data.decode("utf-8"))
-            command_line = command_line + data.decode("utf-8")
-            if len(data.decode("utf-8")) < 16:
-                break
-        print("command_line: " + str(command_line))
-        print(is_a_valid_command_line(command_line))
+        data_received = connection.recv(16)
+        # print("recebido: " + data_received.decode("utf-8"))
+        message_received = message_received + data_received.decode("utf-8")
+        if len(data_received.decode("utf-8")) < 16:
+            break
+    return message_received
+
+
+def control_connection(connection, client_address):
+    print("control connection from" + str(client_address))
+
+    connection.sendall(bytes("user:", 'utf-8'))
+    username = receive_message(connection)
+    print("username recebido: " + str(username))
+
+    connection.sendall(bytes("password:", 'utf-8'))
+    password = receive_message(connection)
+    print("password recebido: " + str(password))
+
+
+# def control_connection(connection, client_address):
+#     print("control connection from" + str(client_address))
+#     connection.sendall(bytes("user:", 'utf-8'))
+#
+#     while True:
+#         command_line = ""
+#         while True:
+#             data = connection.recv(16)
+#             print("recebido: " + data.decode("utf-8"))
+#             command_line = command_line + data.decode("utf-8")
+#             if len(data.decode("utf-8")) < 16:
+#                 break
+#         print("command_line: " + str(command_line))
+#         print(is_a_valid_command_line(command_line))
 
 
 def is_a_valid_command_line(command_line):
@@ -67,7 +91,7 @@ if __name__ == "__main__":
     while True:
         # Wait for a connection
         print('waiting for a connection')
-        _thread.start_new(test_thread, ("oi",))
+        # _thread.start_new(test_thread, ("oi",))
         connection, client_address = sock.accept()
         _thread.start_new_thread(control_connection, (connection, client_address))
 
